f9a3c73a4b383ae10d59f26e676a92a9679eaec2
Marty Sweet <msweet2214@gmail.com>
2016-07-22 10:16:37 +0100

    Improve readability of file

--- README.md
+++ README.md
@@ -9,8 +9,13 @@ files stored to an Amazon S3 bucket, typically by an inbound SES routing rule. S
 Use cases
 ---------
 
-The original use for this project is to get .ZIP and .GZIP attachments from emails stored in S3, extract the archives,
+The original use for this project is to get .ZIP and .GZIP attachments from emails stored in S3 and extract the archives,
 resulting in .XML files, which are then uploaded to another S3 bucket for processing by another service.
 
-The python file doesn't perform security checks on attachments and will ignore anything which does not 
+The Python file doesn't perform security checks on attachments and will ignore anything which does not 
 result in an .XML file. The original email will be deleted after processing takes place.
+
+This type of processing may be very useful for extracting DMARC reports, which are sent by email service providers 
+to an email address specified in the _rua_ section of your DMARC DNS record, see https://dmarc.org/overview/. You 
+will then be left with a bucket of XML DMARC reports which you may wish to process manually or upload to a service such
+as https://dmarcian.com/dmarc-xml/.
--- lambda_email_extractor.py
+++ lambda_email_extractor.py
@@ -9,6 +9,7 @@ import boto3
 print('Loading function')
 
 s3 = boto3.client('s3')
+xmlDir = "/tmp/output/"
 
 
 def lambda_handler(event, context):
@@ -21,49 +22,55 @@ def lambda_handler(event, context):
         waiter.wait(Bucket=bucket, Key=key)
         response = s3.Object(Bucket=bucket, Key=key)
 
-        msg = email.message_from_string(object.get()["Body"].read())
+        # Read the raw text file into a Email Object
+        msg = email.message_from_string(response.get()["Body"].read())
 
         if len(msg.get_payload()) == 2:
-            
-            attachment = msg.get_payload()[1]
-            xmlDir = "/tmp/output/"
-            
+
             # Create directory for XML files (makes debugging easier)
             if os.path.isdir(xmlDir) == False:
                 os.mkdir(xmlDir)
-            
-            # Process filename.zip attachments 
-            if attachment.get_content_type() == "application/x-zip-compressed":
-                print('ZIP')
-                open('/tmp/attachment.zip', 'wb').write(attachment.get_payload(decode=True))
-                with zipfile.ZipFile('/tmp/attachment.zip', "r") as z:
-                    z.extractall(xmlDir)
-            
-            # Process filename.xml.gz attachments (Providers not complying to standards)
-            elif attachment.get_content_type() == "application/gzip":
-                contentdisp = string.split(attachment.get('Content-Disposition'), '=')
-                fname = contentdisp[1].replace('\"','')
-                
-                open('/tmp/'+contentdisp[1], 'wb').write(attachment.get_payload(decode=True))
-                
-                # This assumes we have filename.xml.gz, if we get this wrong, we will just 
-                # ignore the report
-                xmlname = fname[:-3]
-                open(xmlDir+xmlname, 'wb').write(gzip.open('/tmp/'+contentdisp[1], 'rb').read())
-                
-            else:
-                print('Skipping' + attachment.get_content_type())
-                
-                
-            # Put all XML back into S3 (Covers non-compliant cases if a ZIP contains multiple results)
-            import os
-            for file in os.listdir(xmlDir):
-                if file.endswith(".xml"):
-                    print(file) # File name to upload
+
+            # The first attachment
+            attachment = msg.get_payload()[1]
+
+            # Extract the attachment into /tmp/output
+            extract_attachment(attachment)
+
+            # Upload the XML files to S3
+            upload_resulting_files_to_s3()
                 
     except Exception as e:
         print(e)
         print('Error getting object {} from bucket {}. Make sure they exist '
-          'and your bucket is in the same region as this '
-          'function.'.format(key, bucket))
+            'and your bucket is in the same region as this '
+            'function.'.format(key, bucket))
         raise e
+
+
+def extract_attachment(attachment):
+    # Process filename.zip attachments
+    if attachment.get_content_type() == "application/x-zip-compressed":
+        open('/tmp/attachment.zip', 'wb').write(attachment.get_payload(decode=True))
+        with zipfile.ZipFile('/tmp/attachment.zip', "r") as z:
+            z.extractall(xmlDir)
+
+    # Process filename.xml.gz attachments (Providers not complying to standards)
+    elif attachment.get_content_type() == "application/gzip":
+        contentdisp = string.split(attachment.get('Content-Disposition'), '=')
+        fname = contentdisp[1].replace('\"', '')
+        open('/tmp/' + contentdisp[1], 'wb').write(attachment.get_payload(decode=True))
+        # This assumes we have filename.xml.gz, if we get this wrong, we will just
+        # ignore the report
+        xmlname = fname[:-3]
+        open(xmlDir + xmlname, 'wb').write(gzip.open('/tmp/' + contentdisp[1], 'rb').read())
+
+    else:
+        print('Skipping' + attachment.get_content_type())
+
+
+def upload_resulting_files_to_s3():
+    # Put all XML back into S3 (Covers non-compliant cases if a ZIP contains multiple results)
+    for file in os.listdir(xmlDir):
+        if file.endswith(".xml"):
+            print(file)  # File name to upload

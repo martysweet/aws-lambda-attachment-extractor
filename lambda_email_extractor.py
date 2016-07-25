3a9447c03e7723b8439a1cdd8fd38004b161a4ad
Marty Sweet <msweet2214@gmail.com>
2016-07-25 15:02:02 +0100

    fix reading and upload - this works with Lambda!

--- lambda_email_extractor.py
+++ lambda_email_extractor.py
@@ -11,21 +11,24 @@ import urllib
 print('Loading function')
 
 s3 = boto3.client('s3')
+s3r = boto3.resource('s3')
 xmlDir = "/tmp/output/"
 
-outputBucket = "security-dmarc-raw"
-outputPrefix = "xml/" # Should end with /
+outputBucket = "app.ft-security-dmarc-processor-1iuvz31c9plk5"
+outputPrefix = "xml/"  # Should end with /
 
 
 def lambda_handler(event, context):
     bucket = event['Records'][0]['s3']['bucket']['name']
-    key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key']).decode('uft8')
+    key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key']).decode('utf8')
 
     try:
         # Use waiter to ensure the file is persisted
+
         waiter = s3.get_waiter('object_exists')
         waiter.wait(Bucket=bucket, Key=key)
-        response = s3.Object(Bucket=bucket, Key=key)
+
+        response = s3r.Bucket(bucket).Object(key)
 
         # Read the raw text file into a Email Object
         msg = email.message_from_string(response.get()["Body"].read())
@@ -48,6 +51,7 @@ def lambda_handler(event, context):
         else:
             print("Could not see file/attachment.")
 
+        return 0
     except Exception as e:
         print(e)
         print('Error getting object {} from bucket {}. Make sure they exist '
@@ -58,13 +62,15 @@ def lambda_handler(event, context):
 
 def extract_attachment(attachment):
     # Process filename.zip attachments
-    if attachment.get_content_type() == "application/x-zip-compressed":
+    if (attachment.get_content_type() == "application/x-zip-compressed") or \
+            (attachment.get_content_type() == "multipart/zip"):
         open('/tmp/attachment.zip', 'wb').write(attachment.get_payload(decode=True))
         with zipfile.ZipFile('/tmp/attachment.zip', "r") as z:
             z.extractall(xmlDir)
 
     # Process filename.xml.gz attachments (Providers not complying to standards)
-    elif attachment.get_content_type() == "application/gzip":
+    elif (attachment.get_content_type() == "application/gzip") or \
+            (attachment.get_content_type() == "multipart/x-gzip"):
         contentdisp = string.split(attachment.get('Content-Disposition'), '=')
         fname = contentdisp[1].replace('\"', '')
         open('/tmp/' + contentdisp[1], 'wb').write(attachment.get_payload(decode=True))
@@ -74,14 +80,12 @@ def extract_attachment(attachment):
         open(xmlDir + xmlname, 'wb').write(gzip.open('/tmp/' + contentdisp[1], 'rb').read())
 
     else:
-        print('Skipping' + attachment.get_content_type())
+        print('Skipping ' + attachment.get_content_type())
 
 
 def upload_resulting_files_to_s3():
     # Put all XML back into S3 (Covers non-compliant cases if a ZIP contains multiple results)
-    k = s3.Key(outputBucket)
     for fileName in os.listdir(xmlDir):
         if fileName.endswith(".xml"):
             print("Uploading: " + fileName)  # File name to upload
-            k.key = outputPrefix + fileName
-            k.set_contents_from_file(open(xmlDir+'/'+fileName))
+            s3r.meta.client.upload_file(xmlDir+'/'+fileName, outputBucket, outputPrefix+fileName)
